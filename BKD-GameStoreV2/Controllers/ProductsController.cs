1928b2c8228b46a588e62eba0b081aace56d7e79
Yorman Rojas <yormanxrojas@gmail.com>
2022-04-04 21:58:11 -0500

    add products and view

---
+++ BKD-GameStoreV2/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using BKD_GameStoreV2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BKD_GameStoreV2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listProducts = await _context.Products.ToListAsync();
+                return Ok(listProducts);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Products product)
+        {
+            try
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var card = await _context.Products.FindAsync(id);
+                if (card == null)
+                {
+                    return NotFound();
+                }
+                _context.Products.Remove(card);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "delete product successfull" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BKD-GameStoreV2/Migrations/20220405011720_v1.0.0.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BKD_GameStoreV2.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false),
+                    state = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    platform = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
